d5d3b3a7aed091268a3a8abe139b4f15d171b802
DmitriyValetov <dmitriy.valetov@phystech.edu>
2018-10-13 15:04:06 +0300

    initial commit with two hello worlds and bashscripts for compilations

---
+++ hello world with boost/boost_hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
+int main()
+{
+    std::cout << "Hello, world with boost!" << std::endl;
+
+    return 0;
+}
